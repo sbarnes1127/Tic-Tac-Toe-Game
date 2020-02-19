e172813b241aaa53dbb8f0d37513a2316304bbe3
Sierra Barnes <sierrabarnes@Sierras-MacBook-Pro.local>
2020-02-19 10:57:00 -0500

    added functionality so player can click the board and mark either an x or o, and if a space is already marked, it wont be marked again

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
 let cells = document.querySelectorAll('.row div');
 let turns = 0
+let player = ["X", "O"];
 let endGame = false
 
-cells.forEach(function(cell){
+cells.forEach(function (cell) {
     cell.addEventListener("click", Move);
 })
 
-
-
 function Move(e) {
-    if (turns % 2 ==0){
-        e.target.textContent = "X"
-        turns++
+    if (endGame === true) {
+        restartGame();
+        return;
     }
-    else{
-        e.target.textContent = "O"
-        turns ++
+    if (e.target.textContent === "X" || e.target.textContent === "O") {
+        return;
+    }
+    
+        e.target.textContent = player[turns]
+        toggle();
+        checkWin();
+    
+}
+
+
+function toggle() {
+    if (turns === 0) {
+        turns = 1;
+    }
+    else {
+        turns = 0;
+        
     }
 }
+
+
+
